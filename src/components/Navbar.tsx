509f8eed2cc4a0497031a2a452fc714152c61978
AdarhKumar <adarshnit420@gmail.com>
2025-01-04 14:07:55 +0530

    first commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -28,17 +28,6 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16 items-center">
           {/* Logo */}
-          <div className="flex-shrink-0">
-            <Link href="/">
-              <img
-                src="/logomusics.png" // Replace with your logo
-                alt="Logo"
-                className="w-16 h-16"
-              />
-            </Link>
-          </div>
-
-          {/* Menu Button */}
           <div className="flex items-center">
             <button
               onClick={toggleMenu}
@@ -77,6 +66,18 @@ const Navbar = () => {
               )}
             </button>
           </div>
+          {/* <div className="flex-shrink-0">
+            <Link href="/">
+              <img
+                src="/logomusics.png" // Replace with your logo
+                alt="Logo"
+                className="w-16 h-16"
+              />
+            </Link>
+          </div> */}
+
+          {/* Menu Button */}
+         
         </div>
       </div>
 
