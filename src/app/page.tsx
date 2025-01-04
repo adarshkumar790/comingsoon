73591a2ce7d4bbec66571247c1b184695d75a692
AdarhKumar <adarshnit420@gmail.com>
2025-01-04 11:08:49 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,52 +1,55 @@
-"use client";
-import React from "react";
-import { FaInstagram, FaTelegram } from "react-icons/fa";
-import { FaXTwitter } from "react-icons/fa6";
+// pages/index.tsx
 
-const ComingSoon: React.FC = () => {
+import Image from "next/image";
+
+const Home = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-gray-900 via-gray-800 to-gray-900">
-      <div className="text-center px-4">
-        <h1 className="text-white text-5xl md:text-9xl md:text-gray-300 font-extrabold mb-4">
-          COMING SOON
-        </h1>
-        <p className="text-gray-400 text-lg md:text-2xl font-semibold mb-8">
-          We are building something amazing. Stay connected for updates!
-          We are building something amazing. Stay connected for updates!
-          We are building something amazing. Stay connected for updates!
-        </p>
-        <div className="flex justify-center mt-4 space-x-6 mb-8">
-          <a
-            href="https://t.me/BlockLensOrg"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-gray-200 hover:text-white transition duration-300 text-4xl  md:text-5xl"
-            aria-label="Telegram"
-          >
-            <FaTelegram />
-          </a>
-          <a
-            href="http://x.com/BlockLensOrg"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-gray-300 hover:text-white transition duration-300 text-4xl md:text-5xl"
-            aria-label="Twitter"
-          >
-            <FaXTwitter />
-          </a>
-          <a
-            href="https://instagram.com/BlockLensOrg"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-gray-300 hover:text-white transition duration-300 text-4xl md:text-5xl"
-            aria-label="Instagram"
-          >
-            <FaInstagram />
-          </a>
+    <div className="relative w-screen h-screen bg-gradient-to-b from-purple-900 via-purple-800 to-purple-900 flex items-center justify-center overflow-hidden">
+
+      <div className="relative rounded-full border-dotted border-yellow-400 w-[400px] h-[400px] flex items-center justify-center">
+        
+        <div className="absolute w-full h-full current-effect animate-flowing-current"></div>
+
+    
+        <div className="text-center text-white">
+          <h1 className="text-4xl font-bold">THE</h1>
+          <h2 className="text-5xl font-bold">ART OF</h2>
+          <h3 className="text-4xl font-bold">COMPETING</h3>
         </div>
+
+        
+        <div className="absolute w-full h-full">
+          
+          <div className="absolute w-full h-full animate-logo-spin">
+            <img
+              src="/bgmusic.png"
+              alt="Logo 1"
+              className="absolute w-20 h-20 -top-8 left-1/2 transform -translate-x-1/2"
+            />
+          </div>
+        </div>
+      </div>
+
+      
+      <div className="absolute right-0 top-0 w-[50%] h-full z-[-1]">
+  <Image
+    src="/bgmusic.png"
+    alt="Right Background"
+    layout="fill"
+    objectFit="cover"
+    quality={75}
+    className="opacity-50"
+  />
+</div>
+      <div className="absolute right-10 bottom-10">
+        <img
+          src="/musicman1.png"
+          alt="Character"
+          className="w-[350px] h-[400px]"
+        />
       </div>
     </div>
   );
 };
 
-export default ComingSoon;
+export default Home;
